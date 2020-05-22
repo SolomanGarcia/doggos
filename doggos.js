111f3637c9d8b63dcdb3293a9349a2c1a4916862
Soloman Garcia <soloman.garcia@gmail.com>
2020-05-22 10:21:00 -0600

    create addDoggo function

--- doggos.js
+++ doggos.js
@@ -1,13 +1,15 @@
 const BREEDS_URL = "https://dog.ceo/api/breeds/image/random";
 
-fetch(BREEDS_URL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    const img = document.createElement("img");
-    img.src = data.message;
-    img.alt = "Cute doggo";
+function addDoggo() {
+  fetch(BREEDS_URL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const img = document.createElement("img");
+      img.src = data.message;
+      img.alt = "Cute doggo";
 
-    document.querySelector(".doggos").appendChild(img);
-  });
+      document.querySelector(".doggos").appendChild(img);
+    });
+}
