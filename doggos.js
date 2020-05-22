56a82f1504aaff1a5c959a8f4f8fbe4fccb85627
Soloman Garcia <soloman.garcia@gmail.com>
2020-05-22 10:51:34 -0600

    add drop Down

--- doggos.js
+++ doggos.js
@@ -1,17 +1,42 @@
-const BREEDS_URL = "https://dog.ceo/api/breeds/image/random";
+// const BREEDS_URL = "https://dog.ceo/api/breeds/image/random";
 
-function addDoggo() {
-  fetch(BREEDS_URL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      const img = document.createElement("img");
-      img.src = data.message;
-      img.alt = "Cute doggo";
+// function addDoggo() {
+//   // Show loading spinner
 
-      document.querySelector(".doggos").appendChild(img);
-    });
-}
+//   fetch(BREEDS_URL)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       const img = document.createElement("img");
+//       img.src = data.message;
+//       img.alt = "Cute doggo";
 
-document.querySelector(".add-doggo").addEventListener("click", addDoggo);
+//       document.querySelector(".doggos").appendChild(img);
+
+//       // Stop loading spinner
+//     });
+// }
+
+// document.querySelector(".add-doggo").addEventListener("click", addDoggo);
+
+const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
+
+fetch(BREEDS_URL)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    // console.log(data);
+    const breedsObject = data.message;
+    const breedsArray = Object.keys(breedsObject);
+
+    const select = document.querySelector(".breeds");
+
+    for (let i = 0; i < breedsArray.length; i++) {
+      const option = document.createElement("option");
+      option.value = breedsArray[i];
+      option.innerText = breedsArray[i];
+      select.appendChild(option);
+    }
+  });
